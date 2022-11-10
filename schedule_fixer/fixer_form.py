fab5c8bfa4e71c8557673a9df898b22d52b8f3b1
Jack Papel <jackpapel@live.com>
2022-11-09 22:44:44 -0500

    Remove unnecessary instance members

--- schedule_fixer/fixer_form.py
+++ schedule_fixer/fixer_form.py
@@ -27,8 +27,8 @@ class Form:
 
         offset_frame = tk.Frame()
 
-        self.days_offset_entry = tk.Entry()
-        self.hours_offset_entry = tk.Entry()
+        days_offset_entry = tk.Entry()
+        hours_offset_entry = tk.Entry()
 
         file_selector_frame.pack(expand=True)
         offset_frame.pack(expand=True)
@@ -36,14 +36,14 @@ class Form:
         open_button.pack(in_=file_selector_frame, side=tk.RIGHT)
         tk.Label(text="How many days offset is the calendar? (+): early, (-): late")\
             .pack(in_=offset_frame, after=file_selector_frame)
-        self.days_offset_entry.pack(in_=offset_frame)
+        days_offset_entry.pack(in_=offset_frame)
         tk.Label(text="How many hours offset is the calendar? (+): early, (-): late")\
             .pack(in_=offset_frame)
-        self.hours_offset_entry.pack(in_=offset_frame)
+        hours_offset_entry.pack(in_=offset_frame)
         tk.Button(
             self.root,
             text='Fix!',
-            command=lambda: self.try_close_and_fix(self.hours_offset_entry.get(), self.days_offset_entry.get()),
+            command=lambda: self.try_close_and_fix(self.filename, hours_offset_entry.get(), days_offset_entry.get()),
             padx=20,
             pady=5
         ).pack(expand=True, side=tk.BOTTOM)
@@ -51,24 +51,24 @@ class Form:
     def start(self):
         self.root.mainloop()
 
-    def try_close_and_fix(self, offset_hours: str, offset_days: str):
+    def try_close_and_fix(self, filename: str, offset_hours: str, offset_days: str):
         fail = False
         try:
             offset_hours = int(offset_hours)
             offset_days = int(offset_days)
         except ValueError:
             fail = True
-        if not fail and self.filename and offset_hours is not None and offset_days is not None:
-            self.close_and_fix(offset_hours, offset_days)
+        if not fail and filename and offset_hours is not None and offset_days is not None:
+            self.close_and_fix(filename, offset_hours, offset_days)
         else:
             showinfo(
                 title="Error",
                 message="Please fill all fields!"
             )
 
-    def close_and_fix(self, offset_hours, offset_days):
+    def close_and_fix(self, filename: str, offset_hours, offset_days):
         self.root.destroy()
-        fixer.fix(self.filename, offset_days, offset_hours)
+        fixer.fix(filename, offset_days, offset_hours)
         showinfo(
             title="Fixed",
             message="The calendar has now been fixed!"
